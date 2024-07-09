8688d50db7364568555e7c75c84d7b672e264db8
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-07-08 23:02:52 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailuserController } from './controllers/user/DetailUserController'
+
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+import uploadConfig from './config/multer'
+
+const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
+
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
+
+router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated,  new DetailuserController().handle )
+
+
+export { router }; 
